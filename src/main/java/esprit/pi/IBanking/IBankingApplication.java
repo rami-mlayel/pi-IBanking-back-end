2bef146dee8a999b672dd81719677404ede2df00
Rami Mlayel <rmlayel@vermeg.com>
2022-07-19 10:53:51 +0100

    Adding project

---
+++ src/main/java/esprit/pi/IBanking/IBankingApplication.java
@@ -0,0 +1,13 @@
+package esprit.pi.SoftIB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftIBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftIBApplication.class, args);
+	}
+
+}
