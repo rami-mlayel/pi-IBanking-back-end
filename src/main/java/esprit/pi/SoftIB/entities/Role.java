326ae2961b4de024f928a155f9893b367223b21c
Rami Mlayel <rami.mlayel@esprit.tn>
2022-07-28 23:08:58 +0100

    add script to add default data
    Signed-off-by: Rami Mlayel <rami.mlayel@esprit.tn>

--- src/main/java/esprit/pi/SoftIB/entities/Role.java
+++ src/main/java/esprit/pi/SoftIB/entities/Role.java
@@ -17,7 +17,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Role {
 	@Id
-	@GeneratedValue()
 	private Integer id;
 
 	@Enumerated(EnumType.STRING)
---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into roles values (1, "ROLE_ADMIN");
+insert into roles values (2, "ROLE_AGENT");
+insert into roles values (3, "ROLE_CUSTOMER");
+
+insert into users (username, email, password) values ("admin", "admin@admin.com", "password");
+
+insert into user_roles values (1,1);
+
