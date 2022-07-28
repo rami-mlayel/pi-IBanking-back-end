1880cd28212bddaef4e7e854822b87a8e26c01f5
mhamed <mlaabidi@vermeg.com>
2022-07-28 20:59:13 +0100

    removing getters and setters

--- src/main/java/esprit/pi/SoftIB/entities/CreditRisk.java
+++ src/main/java/esprit/pi/SoftIB/entities/CreditRisk.java
@@ -25,59 +25,6 @@ public class CreditRisk {
 	private Float amount;
 	private Integer duration; 
 	private LoanType purpose;
-	public Integer getAge() {
-		return age;
-	}
-	public void setAge(Integer age) {
-		this.age = age;
-	}
-	public Sex getSex() {
-		return sex;
-	}
-	public void setSex(Sex sex) {
-		this.sex = sex;
-	}
-	public Job getJob() {
-		return job;
-	}
-	public void setJob(Job job) {
-		this.job = job;
-	}
-	public Housing getHousing() {
-		return housing;
-	}
-	public void setHousing(Housing housing) {
-		this.housing = housing;
-	}
-	public Float getSaving() {
-		return saving;
-	}
-	public void setSaving(Float saving) {
-		this.saving = saving;
-	}
-	public Float getCurrent() {
-		return current;
-	}
-	public void setCurrent(Float current) {
-		this.current = current;
-	}
-	public Float getAmount() {
-		return amount;
-	}
-	public void setAmount(Float amount) {
-		this.amount = amount;
-	}
-	public Integer getDuration() {
-		return duration;
-	}
-	public void setDuration(Integer duration) {
-		this.duration = duration;
-	}
-	public LoanType getPurpose() {
-		return purpose;
-	}
-	public void setPurpose(LoanType purpose) {
-		this.purpose = purpose;
-	}
+
 
 }
