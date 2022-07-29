53c492b38c5359c86b748a267995ad2593f769ab
mhamed <mlaabidi@vermeg.com>
2022-07-29 16:56:05 +0100

    service and controller credit risk by loan request

--- src/main/java/esprit/pi/SoftIB/services/CreditRiskServiceImpl.java
+++ src/main/java/esprit/pi/SoftIB/services/CreditRiskServiceImpl.java
@@ -3,31 +3,38 @@ package esprit.pi.SoftIB.services;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.time.LocalDate;
+import java.time.Period;
 import java.io.IOException;
 import java.net.URI;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.time.ZoneId;
+
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import esprit.pi.SoftIB.entities.Account;
 import esprit.pi.SoftIB.entities.CreditRisk;
+import esprit.pi.SoftIB.entities.LoanRequest;
+import esprit.pi.SoftIB.enumeration.AccountType;
 import esprit.pi.SoftIB.enumeration.Housing;
 import esprit.pi.SoftIB.enumeration.Job;
 import esprit.pi.SoftIB.enumeration.LoanType;
 import esprit.pi.SoftIB.enumeration.Sex;
 import esprit.pi.SoftIB.repository.LoanRequestRepository;
-import esprit.pi.SoftIB.repository.UserRepository;
 
 @Service
 public class CreditRiskServiceImpl implements ICreditRiskService {
 	@Autowired
 	private LoanRequestRepository loanRequestRepository;
 	@Override
-	public String GetRisk(CreditRisk creditRisk) throws IOException, InterruptedException {
+	public String getRisk(CreditRisk creditRisk) throws IOException, InterruptedException {
 		List<Integer> predictionData = new ArrayList<>();
 		predictionData.add(creditRisk.getAge());
 		if(creditRisk.getSex()==Sex.MALE) {	predictionData.add(1);}
@@ -68,7 +75,32 @@ public class CreditRiskServiceImpl implements ICreditRiskService {
                 HttpResponse.BodyHandlers.ofString());
 
 		
-		return response.body()+"%";
+		return response.body().concat("%");
+	}
+	@Override
+	public String getRiskByLoanRequestId(Long id) throws IOException, InterruptedException {
+		LoanRequest loanRequest= loanRequestRepository.findById(id).get();
+		CreditRisk risk= new CreditRisk();
+		Date userBD=loanRequest.getAccount().getUserAccount().getCustomer().getBirthDate();
+		LocalDate today= LocalDate.now();
+		LocalDate date= userBD.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+		risk.setAge(Period.between(date, today).getYears());
+		risk.setSex(loanRequest.getAccount().getUserAccount().getCustomer().getSex());
+		risk.setJob(loanRequest.getAccount().getUserAccount().getCustomer().getJobStatus());
+		risk.setHousing(loanRequest.getHousing());
+		List<Account> accounts =loanRequest.getAccount().getUserAccount().getCustomer().getUser().getAccounts() ;
+		Float savings=0F;
+		Float currents=0F;
+		for(Account account : accounts) {
+			if( account.getType()==AccountType.SAVING_ACCOUNT) {savings=savings+account.getBalance().floatValue();}
+			if( account.getType()==AccountType.CURRENT_ACCOUNT) {currents=currents+account.getBalance().floatValue();}
+		}
+		risk.setSaving(savings);
+		risk.setCurrent(currents);
+		risk.setAmount(loanRequest.getSum().floatValue());
+		risk.setDuration(loanRequest.getMonthDuration());
+		risk.setPurpose(loanRequest.getPurpose());
+		return this.getRisk(risk);
 	}
 
 }
--- src/main/java/esprit/pi/SoftIB/services/ICreditRiskService.java
+++ src/main/java/esprit/pi/SoftIB/services/ICreditRiskService.java
@@ -5,12 +5,8 @@ import java.io.IOException;
 import com.fasterxml.jackson.core.JsonProcessingException;
 
 import esprit.pi.SoftIB.entities.CreditRisk;
-import esprit.pi.SoftIB.enumeration.Housing;
-import esprit.pi.SoftIB.enumeration.Job;
-import esprit.pi.SoftIB.enumeration.LoanType;
-import esprit.pi.SoftIB.enumeration.Sex;
 
 public interface ICreditRiskService {
-	String GetRisk(CreditRisk creditRisk) throws JsonProcessingException, IOException, InterruptedException;
-
+	String getRisk(CreditRisk creditRisk) throws JsonProcessingException, IOException, InterruptedException;
+    String getRiskByLoanRequestId(Long id) throws IOException, InterruptedException;
 }
